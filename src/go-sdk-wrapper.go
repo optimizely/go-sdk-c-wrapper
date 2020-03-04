87fea04106096fe25446174d055c7c30150a160b
Ola Nordstrom <ola.nordstrom@optimizely.com>
2020-03-03 21:27:30 -0800

    more cleanup for blog

--- src/go-sdk-wrapper.go
+++ src/go-sdk-wrapper.go
@@ -20,11 +20,17 @@ typedef struct optimizely_user_attributes{
     char *id;
     struct optimizely_user_attribute *user_attribute_list;
 } optimizely_user_attributes;
+
+typedef struct optimizely_sdk_feature_variable_boolean {
+	char *err;
+	_Bool rv;
+} optimizely_sdk_feature_variable_boolean;
 */
 import "C"
 
 import (
 	"errors"
+	"fmt"
 	optly "github.com/optimizely/go-sdk"
 	"github.com/optimizely/go-sdk/pkg/client"
 	"github.com/optimizely/go-sdk/pkg/entities"
@@ -64,12 +70,15 @@ func optimizely_sdk_init() uint32 {
 // important: caller must free error string
 //export optimizely_sdk_get_error
 func optimizely_sdk_get_error() *C.char {
-	s := optlyErr.Error()
-	c_str := C.CString(s) // this allocates a string, caller must free it
-	return c_str
+	if optlyErr != nil {
+		s := optlyErr.Error()
+		c_str := C.CString(s) // this allocates a string, caller must free it
+		return c_str
+	}
+	return nil
 }
 
-// export optimizely_sdk_free
+//export optimizely_sdk_free
 func optimizely_sdk_free(str *C.char) {
 	C.free(unsafe.Pointer(str))
 }
@@ -104,13 +113,14 @@ func optimizelySdkClient(sdkkey string) int32 {
 
 //export optimizely_sdk_delete_client
 func optimizely_sdk_delete_client(handle int32) {
+	optimizely_sdk_close(handle)
 	optlyClients.lock.Lock()
 	delete(optlyClients.m, handle)
 	optlyClients.lock.Unlock()
 }
 
 //export optimizely_sdk_is_feature_enabled
-func optimizely_sdk_is_feature_enabled(handle int32, feature_name *C.char, attributes_list C.struct_optimizely_user_attributes) int32 {
+func optimizely_sdk_is_feature_enabled(handle int32, feature_name *C.char, attribs C.struct_optimizely_user_attributes, errno *C.int) int32 {
 	optlyClients.lock.RLock()
 	optlyClient, ok := optlyClients.m[handle]
 	optlyClients.lock.RUnlock()
@@ -118,14 +128,17 @@ func optimizely_sdk_is_feature_enabled(handle int32, feature_name *C.char, attri
 		optlyErr = errors.New("no client exists with the specified handle id")
 		return -1
 	}
+	fmt.Printf("in optimizely_sdk_is_feature_enabled, errno is: %v derefrenced: %v\n", errno, &errno)
 
-	// TODO loop through the attributes in the attributes_list and initialize the UserContext Attributes map
-	u := entities.UserContext{ID: C.GoString(attributes_list.id)}
+	// TODO loop through the attributes in the attributes and initialize the UserContext Attributes map
+	u := entities.UserContext{ID: C.GoString(attribs.id)}
 
 	enabled, err := optlyClient.IsFeatureEnabled(C.GoString(feature_name), u)
 
 	if err != nil {
+		fmt.Printf("errors is not nil, it is: %v\n", err)
 		optlyErr = err
+		//		*errno = 1
 		return -1
 	}
 
@@ -145,15 +158,26 @@ func optimizelySdkIsFeatureEnabled(handle int32, featureName string, userCtx ent
 	}
 
 	// TODO loop through the user_context and create the rest of the attribs
-	rv := optimizely_sdk_is_feature_enabled(handle, feature_name, attribs)
+
+	//var errno *C.int
+	/*
+		errno := C.malloc(C.sizeof_int)
+		rv := optimizely_sdk_is_feature_enabled(handle, feature_name, attribs, (*C.int)(errno))
+	*/
+
+	var errno2 C.int
+	errno2 = 0
+	//fmt.Printf("in optimizelySdkIsFeatureEnabled, errno is: %v derefrenced: %v\n", errno2, &errno2)
+	rv := optimizely_sdk_is_feature_enabled(handle, feature_name, attribs, &errno2)
 
 	C.free(unsafe.Pointer(feature_name))
 	C.free(unsafe.Pointer(user))
 	return rv
 }
 
+/*
 //export optimizely_sdk_get_feature_variable_string
-func optimizely_sdk_get_feature_variable_string(handle int32, feature_name *C.char, variable_key *C.char, user *C.char) *C.char {
+func optimizely_sdk_get_feature_variable_string(handle int32, feature_name *C.char, variable_key *C.char, attribs C.struct_optimizely_user_attributes) *C.char {
 	optlyClients.lock.RLock()
 	optlyClient, ok := optlyClients.m[handle]
 	optlyClients.lock.RUnlock()
@@ -162,7 +186,7 @@ func optimizely_sdk_get_feature_variable_string(handle int32, feature_name *C.ch
 		return nil
 	}
 
-	u := entities.UserContext{ID: C.GoString(user)}
+	u := entities.UserContext{ID: C.GoString(attribs.id)}
 	s, err := optlyClient.GetFeatureVariableString(C.GoString(feature_name), C.GoString(variable_key), u)
 	if err != nil {
 		optlyErr = err
@@ -170,6 +194,396 @@ func optimizely_sdk_get_feature_variable_string(handle int32, feature_name *C.ch
 	}
 
 	return C.CString(s) // caller must free string
+}
+
+func optimizelySdkGetFeatureVariableString(handle int32, featureName string, variableKey string, userCtx entities.UserContext) string {
+	feature_name := C.CString(featureName)
+	variable_key := C.CString(variableKey)
+	user := C.CString(userCtx.ID)
+	attribs := C.struct_optimizely_user_attributes{
+		id:                  user,
+		user_attribute_list: nil,
+	}
+
+	// TODO loop through the user_context and create the rest of the attribs
+	s := optimizely_sdk_get_feature_variable_string(handle, feature_name, variable_key, attribs)
+	str := C.GoString(s)
+
+	C.free(unsafe.Pointer(feature_name))
+	C.free(unsafe.Pointer(variable_key))
+	C.free(unsafe.Pointer(user))
+	C.free(unsafe.Pointer(s))
+
+	return str
+}
+*/
+
+//export optimizely_sdk_get_feature_variable_string
+func optimizely_sdk_get_feature_variable_string(handle int32, feature_name *C.char, variable_key *C.char, attribs C.struct_optimizely_user_attributes, err **C.char) *C.char {
+	optlyClients.lock.RLock()
+	optlyClient, ok := optlyClients.m[handle]
+	optlyClients.lock.RUnlock()
+	if !ok {
+		cstr := C.CString("no client exists with the specified handle id") // this allocates a string, caller must free it
+		*err = cstr
+		return nil
+	}
+
+	// TODO get rest of the attributes
+	u := entities.UserContext{ID: C.GoString(attribs.id)}
+	s, e := optlyClient.GetFeatureVariableString(C.GoString(feature_name), C.GoString(variable_key), u)
+	if e != nil {
+		*err = C.CString(e.Error())
+		return nil
+	}
+	*err = nil
+
+	return C.CString(s) // caller must free
+}
+
+func optimizelySdkGetFeatureVariableString(handle int32, featureKey string, variableKey string, userCtx entities.UserContext) (string, error) {
+	feature_name := C.CString(featureKey)
+	variable_key := C.CString(variableKey)
+	user := C.CString(userCtx.ID)
+	attribs := C.struct_optimizely_user_attributes{
+		id:                  user,
+		user_attribute_list: nil,
+	}
+
+	// TODO loop through the user_context and create the rest of the attribs
+	var err *C.char
+	var e error
+	s := optimizely_sdk_get_feature_variable_string(handle, feature_name, variable_key, attribs, &err)
+	if err != nil {
+		e = errors.New(C.GoString(err))
+	} else {
+		e = nil
+	}
+
+	C.free(unsafe.Pointer(feature_name))
+	C.free(unsafe.Pointer(variable_key))
+	C.free(unsafe.Pointer(user))
+
+	str := C.GoString(s)
+	C.free(unsafe.Pointer(s))
+
+	return str, e
+}
+
+//export optimizely_sdk_get_feature_variable_boolean
+func optimizely_sdk_get_feature_variable_boolean(handle int32, feature_name *C.char, variable_key *C.char, attribs C.struct_optimizely_user_attributes, err **C.char) bool {
+	optlyClients.lock.RLock()
+	optlyClient, ok := optlyClients.m[handle]
+	optlyClients.lock.RUnlock()
+	if !ok {
+		cstr := C.CString("no client exists with the specified handle id") // this allocates a string, caller must free it
+		*err = cstr
+		return false
+	}
+
+	// TODO get rest of the attributes
+	u := entities.UserContext{ID: C.GoString(attribs.id)}
+	b, e := optlyClient.GetFeatureVariableBoolean(C.GoString(feature_name), C.GoString(variable_key), u)
+	if e != nil {
+		*err = C.CString(e.Error())
+		return false
+	}
+	*err = nil
+
+	return b
+}
+
+func optimizelySdkGetFeatureVariableBoolean(handle int32, featureKey string, variableKey string, userCtx entities.UserContext) (bool, error) {
+	feature_name := C.CString(featureKey)
+	variable_key := C.CString(variableKey)
+	user := C.CString(userCtx.ID)
+	attribs := C.struct_optimizely_user_attributes{
+		id:                  user,
+		user_attribute_list: nil,
+	}
+
+	// TODO loop through the user_context and create the rest of the attribs
+	var err *C.char
+	var e error
+	rv := optimizely_sdk_get_feature_variable_boolean(handle, feature_name, variable_key, attribs, &err)
+	if err != nil {
+		e = errors.New(C.GoString(err))
+	} else {
+		e = nil
+	}
+
+	C.free(unsafe.Pointer(feature_name))
+	C.free(unsafe.Pointer(variable_key))
+	C.free(unsafe.Pointer(user))
+
+	return rv, e
+}
+
+//export optimizely_sdk_get_feature_variable_double
+func optimizely_sdk_get_feature_variable_double(handle int32, feature_name *C.char, variable_key *C.char, attribs C.struct_optimizely_user_attributes, err **C.char) float64 {
+	optlyClients.lock.RLock()
+	optlyClient, ok := optlyClients.m[handle]
+	optlyClients.lock.RUnlock()
+	if !ok {
+		cstr := C.CString("no client exists with the specified handle id") // this allocates a string, caller must free it
+		*err = cstr
+		return 0
+	}
+
+	// TODO get rest of the attributes
+	u := entities.UserContext{ID: C.GoString(attribs.id)}
+	d, e := optlyClient.GetFeatureVariableDouble(C.GoString(feature_name), C.GoString(variable_key), u)
+	if e != nil {
+		*err = C.CString(e.Error())
+		return 0
+	}
+	*err = nil
+
+	return d
+}
+
+func optimizelySdkGetFeatureVariableDouble(handle int32, featureKey string, variableKey string, userCtx entities.UserContext) (float64, error) {
+	feature_name := C.CString(featureKey)
+	variable_key := C.CString(variableKey)
+	user := C.CString(userCtx.ID)
+	attribs := C.struct_optimizely_user_attributes{
+		id:                  user,
+		user_attribute_list: nil,
+	}
+
+	// TODO loop through the user_context and create the rest of the attribs
+	var err *C.char
+	var e error
+	rv := optimizely_sdk_get_feature_variable_double(handle, feature_name, variable_key, attribs, &err)
+	if err != nil {
+		e = errors.New(C.GoString(err))
+	} else {
+		e = nil
+	}
+
+	C.free(unsafe.Pointer(feature_name))
+	C.free(unsafe.Pointer(variable_key))
+	C.free(unsafe.Pointer(user))
+
+	return rv, e
+}
+
+//func optimizelySdkGetFeatureVariableInteger(featureKey string, variableKey string, userContext entities.UserContext) : int, error
+
+//export optimizely_sdk_get_feature_variable_integer
+func optimizely_sdk_get_feature_variable_integer(handle int32, feature_name *C.char, variable_key *C.char, attribs C.struct_optimizely_user_attributes, err **C.char) int {
+	optlyClients.lock.RLock()
+	optlyClient, ok := optlyClients.m[handle]
+	optlyClients.lock.RUnlock()
+	if !ok {
+		cstr := C.CString("no client exists with the specified handle id") // this allocates a string, caller must free it
+		*err = cstr
+		return 0
+	}
+
+	// TODO get rest of the attributes
+	u := entities.UserContext{ID: C.GoString(attribs.id)}
+	i, e := optlyClient.GetFeatureVariableInteger(C.GoString(feature_name), C.GoString(variable_key), u)
+	if e != nil {
+		*err = C.CString(e.Error())
+		return 0
+	}
+	*err = nil
+
+	return i
+}
+
+func optimizelySdkGetFeatureVariableInteger(handle int32, featureKey string, variableKey string, userCtx entities.UserContext) (int, error) {
+	feature_name := C.CString(featureKey)
+	variable_key := C.CString(variableKey)
+	user := C.CString(userCtx.ID)
+	attribs := C.struct_optimizely_user_attributes{
+		id:                  user,
+		user_attribute_list: nil,
+	}
+
+	// TODO loop through the user_context and create the rest of the attribs
+	var err *C.char
+	var e error
+	rv := optimizely_sdk_get_feature_variable_integer(handle, feature_name, variable_key, attribs, &err)
+	if err != nil {
+		e = errors.New(C.GoString(err))
+	} else {
+		e = nil
+	}
+
+	C.free(unsafe.Pointer(feature_name))
+	C.free(unsafe.Pointer(variable_key))
+	C.free(unsafe.Pointer(user))
+
+	return rv, e
+}
+
+//export optimizely_sdk_get_variation
+func optimizely_sdk_get_variation(handle int32, experiment_key *C.char, attribs C.struct_optimizely_user_attributes, err **C.char) *C.char {
+	optlyClients.lock.RLock()
+	optlyClient, ok := optlyClients.m[handle]
+	optlyClients.lock.RUnlock()
+	if !ok {
+		cstr := C.CString("no client exists with the specified handle id") // this allocates a string, caller must free it
+		*err = cstr
+		return nil
+	}
+
+	// TODO get rest of the attributes
+	u := entities.UserContext{ID: C.GoString(attribs.id)}
+	s, e := optlyClient.GetVariation(C.GoString(experiment_key), u)
+	if e != nil {
+		*err = C.CString(e.Error())
+		return nil
+	}
+	*err = nil
+
+	return C.CString(s)
+}
+
+func optimizelySdkGetVariation(handle int32, experimentKey string, variableKey string, userCtx entities.UserContext) (string, error) {
+	experiment_key := C.CString(experimentKey)
+	user := C.CString(userCtx.ID)
+	attribs := C.struct_optimizely_user_attributes{
+		id:                  user,
+		user_attribute_list: nil,
+	}
+
+	// TODO loop through the user_context and create the rest of the attribs
+	var err *C.char
+	var e error
+	s := optimizely_sdk_get_variation(handle, experiment_key, attribs, &err)
+	if err != nil {
+		e = errors.New(C.GoString(err))
+	} else {
+		e = nil
+	}
+
+	C.free(unsafe.Pointer(experiment_key))
+	C.free(unsafe.Pointer(user))
+
+	str := C.GoString(s)
+	C.free(unsafe.Pointer(s))
+
+	return str, e // caller must free string
+}
+
+//export optimizely_sdk_get_feature_variable
+func optimizely_sdk_get_feature_variable(handle int32, feature_name *C.char, variable_key *C.char, attribs C.struct_optimizely_user_attributes, variable_type **C.char, err **C.char) *C.char {
+	optlyClients.lock.RLock()
+	optlyClient, ok := optlyClients.m[handle]
+	optlyClients.lock.RUnlock()
+	if !ok {
+		cstr := C.CString("no client exists with the specified handle id") // this allocates a string, caller must free it
+		*err = cstr
+		return nil
+	}
+
+	// TODO get rest of the attributes
+	u := entities.UserContext{ID: C.GoString(attribs.id)}
+	s, varType, e := optlyClient.GetFeatureVariable(C.GoString(feature_name), C.GoString(variable_key), u)
+	if e != nil {
+		*err = C.CString(e.Error())
+		return nil
+	}
+	*variable_type = C.CString(string(varType))
+	*err = nil
+
+	return C.CString(s) // caller must free
+}
+
+func optimizelySdkGetFeatureVariable(handle int32, featureKey string, variableKey string, userCtx entities.UserContext) (string, string /*entities.VariableType*/, error) {
+	feature_name := C.CString(featureKey)
+	variable_key := C.CString(variableKey)
+	user := C.CString(userCtx.ID)
+	attribs := C.struct_optimizely_user_attributes{
+		id:                  user,
+		user_attribute_list: nil,
+	}
+
+	// TODO loop through the user_context and create the rest of the attribs
+	var err *C.char
+	var var_type *C.char
+	var e error
+	s := optimizely_sdk_get_feature_variable(handle, feature_name, variable_key, attribs, &var_type, &err)
+	if err != nil {
+		e = errors.New(C.GoString(err))
+	} else {
+		e = nil
+	}
+
+	C.free(unsafe.Pointer(feature_name))
+	C.free(unsafe.Pointer(variable_key))
+	C.free(unsafe.Pointer(user))
+
+	str := C.GoString(s)
+	C.free(unsafe.Pointer(s))
+
+	str2 := C.GoString(var_type)
+	C.free(unsafe.Pointer(var_type))
+
+	return str, str2, e
+}
+
+// func (o *OptimizelyClient) Activate(experimentKey string, userContext entities.UserContext) (result string, err error) {
+
+//export optimizely_sdk_activate
+func optimizely_sdk_activate(handle int32, experiment_key *C.char, attribs C.struct_optimizely_user_attributes, err **C.char) *C.char {
+	optlyClients.lock.RLock()
+	optlyClient, ok := optlyClients.m[handle]
+	optlyClients.lock.RUnlock()
+	if !ok {
+		cstr := C.CString("no client exists with the specified handle id") // this allocates a string, caller must free it
+		*err = cstr
+		return nil
+	}
+
+	// TODO get rest of the attributes
+	u := entities.UserContext{ID: C.GoString(attribs.id)}
+	s, e := optlyClient.Activate(C.GoString(experiment_key), u)
+	if e != nil {
+		*err = C.CString(e.Error())
+		return nil
+	}
+	*err = nil
+
+	return C.CString(s) // caller must free
+}
+
+func optimizelySdkActivate(handle int32, experimentKey string, userCtx entities.UserContext) (string, error) {
+	experiment_key := C.CString(experimentKey)
+	user := C.CString(userCtx.ID)
+	attribs := C.struct_optimizely_user_attributes{
+		id:                  user,
+		user_attribute_list: nil,
+	}
+
+	// TODO loop through the user_context and create the rest of the attribs
+	var err *C.char
+	var e error
+	s := optimizely_sdk_activate(handle, experiment_key, attribs, &err)
+	if err != nil {
+		e = errors.New(C.GoString(err))
+	} else {
+		e = nil
+	}
+
+	C.free(unsafe.Pointer(experiment_key))
+
+	str := C.GoString(s)
+	C.free(unsafe.Pointer(s))
+
+	return str, e
+}
+
+func optimizely_sdk_close(handle int32) {
+	optlyClients.lock.RLock()
+	optlyClient, ok := optlyClients.m[handle]
+	optlyClients.lock.RUnlock()
+	if ok {
+		optlyClient.Close()
+	}
 }
 
 func main() {
--- src/wrapper_test.go
+++ src/wrapper_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/optimizely/go-sdk/pkg/entities"
+	"os"
 	"testing"
 )
 
@@ -10,8 +12,8 @@ func TestClientInit(t *testing.T) {
 		t.Fatal("Failed to initialize the SDK")
 	}
 
-	c1 := optimizelySdkClient("some fake key")
-	c2 := optimizelySdkClient("some fake key")
+	c1 := optimizelySdkClient(os.Getenv("OPTIMIZELY_SDKKEY"))
+	c2 := optimizelySdkClient(os.Getenv("OPTIMIZELY_SDKKEY"))
 	fmt.Printf("c1: %v c2: %v\n", c1, c2)
 	if c1 == c2 {
 		t.Fatal("different client instances not returned")
@@ -23,11 +25,84 @@ func TestClientDelete(t *testing.T) {
 		t.Fatal("Failed to initialize the SDK")
 	}
 
-	h := optimizelySdkClient("some fake key")
+	h := optimizelySdkClient(os.Getenv("OPTIMIZELY_SDKKEY"))
 	optimizely_sdk_delete_client(h)
-	rv := optimizelySdkIsFeatureEnabled(h, "", "")
+
+	u := entities.UserContext{ID: "i'm a user Id"}
+	rv := optimizelySdkIsFeatureEnabled(h, "", u)
 	if rv != -1 {
 		t.Fatal("invalid handle error not returned")
 	}
+}
 
+func TestGetFeatureVariableBoolen(t *testing.T) {
+	if optimizely_sdk_init() != 0 {
+		t.Fatal("Failed to initialize the SDK")
+	}
+
+	h := optimizelySdkClient(os.Getenv("OPTIMIZELY_SDKKEY"))
+
+	u := entities.UserContext{ID: os.Getenv("OPTIMIZELY_END_USER_ID")}
+	rv, err := optimizelySdkGetFeatureVariableBoolean(h, os.Getenv("OPTIMIZELY_FEATURE_NAME"), "boolvar", u)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// free all the sturct
+	optimizely_sdk_delete_client(h)
+	t.Log("optimizelySdkGetFeatureVariableBoolean: ", rv)
+}
+
+func TestGetFeatureVariableString(t *testing.T) {
+	if optimizely_sdk_init() != 0 {
+		t.Fatal("Failed to initialize the SDK")
+	}
+
+	h := optimizelySdkClient(os.Getenv("OPTIMIZELY_SDKKEY"))
+
+	u := entities.UserContext{ID: os.Getenv("OPTIMIZELY_END_USER_ID")}
+	rv, err := optimizelySdkGetFeatureVariableString(h, os.Getenv("OPTIMIZELY_FEATURE_NAME"), "stringvar", u)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// free all the sturct
+	optimizely_sdk_delete_client(h)
+	t.Log("optimizelySdkGetFeatureVariableDouble: ", rv)
+}
+
+func TestGetFeatureVariableDouble(t *testing.T) {
+	if optimizely_sdk_init() != 0 {
+		t.Fatal("Failed to initialize the SDK")
+	}
+
+	h := optimizelySdkClient(os.Getenv("OPTIMIZELY_SDKKEY"))
+
+	u := entities.UserContext{ID: os.Getenv("OPTIMIZELY_END_USER_ID")}
+	rv, err := optimizelySdkGetFeatureVariableDouble(h, os.Getenv("OPTIMIZELY_FEATURE_NAME"), "doublevar", u)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// free all the sturct
+	optimizely_sdk_delete_client(h)
+	t.Log("optimizelySdkGetFeatureVariableDouble: ", rv)
+}
+
+func TestGetFeatureVariableInteger(t *testing.T) {
+	if optimizely_sdk_init() != 0 {
+		t.Fatal("Failed to initialize the SDK")
+	}
+
+	h := optimizelySdkClient(os.Getenv("OPTIMIZELY_SDKKEY"))
+
+	u := entities.UserContext{ID: os.Getenv("OPTIMIZELY_END_USER_ID")}
+	rv, err := optimizelySdkGetFeatureVariableInteger(h, os.Getenv("OPTIMIZELY_FEATURE_NAME"), "integervar", u)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// free all the sturct
+	optimizely_sdk_delete_client(h)
+	t.Log("optimizelySdkGetFeatureVariableInteger: ", rv)
 }
