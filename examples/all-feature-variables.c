354b28285cb24e7e33c87e82aaf00c2737b2fcdf
Ola Nordstrom <ola.nordstrom@optimizely.com>
2020-03-19 15:35:45 -0700

    updated compiler documentation

--- examples/all-feature-variables.c
+++ examples/all-feature-variables.c
@@ -8,7 +8,7 @@
  *        Version: 1.0
  *        Created: 02/04/2020 15:31:46
  *       Revision: none
- *       Compiler: gcc
+ *       Compiler: clang
  *
  *         Author: Ola Nordstrom (ola.nordstrom@optimizely.com)
  *   Organization: Optimizely 
